a175a8e246bdec12e1b34bd08adb09c6587e25e5
HuberttGit <hubert.talib@gmail.com>
2023-10-06 23:08:32 +0200

    static private variables

--- src/lib/server/admin.ts
+++ src/lib/server/admin.ts
@@ -1,15 +1,15 @@
 import { getAuth } from 'firebase-admin/auth';
 import { getFirestore } from 'firebase-admin/firestore';
-// import { FB_CLIENT_EMAIL, FB_PRIVATE_KEY, FB_PROJECT_ID } from '$env/static/private';
-import { env } from '$env/dynamic/private';
+import { FB_CLIENT_EMAIL, FB_PRIVATE_KEY, FB_PROJECT_ID } from '$env/static/private';
+// import { env } from '$env/dynamic/private';
 import pkg from 'firebase-admin';
 
 try {
 	pkg.initializeApp({
 		credential: pkg.credential.cert({
-			projectId: env.FB_PROJECT_ID,
-			clientEmail: env.FB_CLIENT_EMAIL,
-			privateKey: env.FB_PRIVATE_KEY
+			projectId: FB_PROJECT_ID,
+			clientEmail: FB_CLIENT_EMAIL,
+			privateKey: FB_PRIVATE_KEY
 		})
 	});
 } catch (err: any) {
